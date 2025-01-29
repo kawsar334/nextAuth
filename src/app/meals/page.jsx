94923f27cbf9da2ee25d96bb27f27ac62d3ce6b2
Arman <mdarman365506@gmail.com>
2025-01-29 17:10:58 +0600

    added new font

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -2,6 +2,13 @@
 import Link from "next/link";
 import MealSearchInput from "./components/MealSearchInput";
 import Image from "next/image";
+import { Roboto } from "next/font/google";
+
+
+const roboto = Roboto({
+  weight: ["400",  "700"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "All Meals",
@@ -40,8 +47,13 @@ export default async function MealsPage({searchParams}) {
       <div className="grid grid-cols-4 gap-4">
         {meals?.map((singleMeal) => {
           return (
-            <div key={singleMeal.idMeal}>
-              <Image src={singleMeal.strMealThumb} width={640} height={640} alt="" />
+            <div key={singleMeal.idMeal} className={`${roboto.className}`}>
+              <Image
+                src={singleMeal.strMealThumb}
+                width={640}
+                height={640}
+                alt=""
+              />
               <p>{singleMeal?.strMeal}</p>
               <p>{singleMeal?.strInstructions}</p>
               <Link href={`/meals/${singleMeal.idMeal}`}>
