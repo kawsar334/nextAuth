a0702b1b7b5381763d93e1d11dd11b3048b629a0
Arman <mdarman365506@gmail.com>
2025-02-03 16:07:45 +0600

    added loading on products page

---
+++ src/app/products/loading.jsx
@@ -0,0 +1,11 @@
+
+
+const loading = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <p>Loading....</p>
+        </div>
+    );
+};
+
+export default loading;
