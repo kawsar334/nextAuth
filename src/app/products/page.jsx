5c893e051a9898ae6ef21a3c401b82a293f8b168
Arman <mdarman365506@gmail.com>
2025-02-06 23:01:25 +0600

    working after deploy

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,28 +2,25 @@
 import React from 'react'
 
 export default async function ProductsPage() {
-    // const res = await fetch(
-    //   `https://bistro-boss-server-indol-five.vercel.app/api/items`,
-    //   {
-    //     cache: "force-cache",
-    //   }
-    // );
-    // const data = await res.json();
+    const res = await fetch(
+      `https://nextjs-data-fetching-2mzd.vercel.app/api/items`,
+      {
+        cache: "force-cache",
+      }
+    );
+    const data = await res.json();
 
     // if (data?.data?.length > 3) {
     //     redirect('/')
     // }
 
   return (
-    //   <ul className='text-center mt-8'>
-    //       {data?.data?.map((singleProduct) => {
-    //           return <li key={singleProduct._id}>{ singleProduct?.productName}</li>
-    //       })
-    //       }
+      <ul className='text-center mt-8'>
+          {data?.data?.map((singleProduct) => {
+              return <li key={singleProduct._id}>{ singleProduct?.productName}</li>
+          })
+          }
           
-    // </ul>
-    <div>
-      <p>hi</p>
-    </div>
+    </ul>
   )
 }
