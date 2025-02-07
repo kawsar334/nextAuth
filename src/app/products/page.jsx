96541ff6bbbad411bbe7f5b79978660532391137
Arman <mdarman365506@gmail.com>
2025-02-07 11:05:30 +0600

    working on deploy

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,27 +1,22 @@
 // import { redirect } from 'next/navigation';
-import React from 'react'
-// 
+import React from "react";
+// /
 export default async function ProductsPage() {
   const { NEXT_PUBLIC_SERVER_ADDRESS } = process.env;
-    const res = await fetch(
-      `${NEXT_PUBLIC_SERVER_ADDRESS}/api/items`,
-      {
-        cache: "force-cache",
-      }
-    );
-    const data = await res.json();
+  const res = await fetch(`${NEXT_PUBLIC_SERVER_ADDRESS}/api/items`, {
+    cache: "force-cache",
+  });
+  const data = await res.json();
 
-    // if (data?.data?.length > 3) {
-    //     redirect('/')
-    // }
+  // if (data?.data?.length > 3) {
+  //     redirect('/')
+  // }
 
   return (
-      <ul className='text-center mt-8'>
-          {data?.data?.map((singleProduct) => {
-              return <li key={singleProduct._id}>{ singleProduct?.productName}</li>
-          })
-          }
-          
+    <ul className="text-center mt-8">
+      {data?.data?.map((singleProduct) => {
+        return <li key={singleProduct._id}>{singleProduct?.productName}</li>;
+      })}
     </ul>
-  )
+  );
 }
