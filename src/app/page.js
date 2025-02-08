fd444c27828e32f3d17ea102f70e3ceb3d61229c
Arman <mdarman365506@gmail.com>
2025-02-08 22:01:53 +0600

    some problem

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import LoginBtn from "@/components/LoginBtn";
+import UserInfo from "../../nextjs-data-fetching/src/components/UserInfo";
 
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
       
-      <LoginBtn/>
+      <LoginBtn />
+      <UserInfo/>
    </div>
   );
 }
